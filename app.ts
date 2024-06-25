b03222e359b9eafa04c86d5238dccbdb82290d5d
Deniss Kozickis <deniss.kozickis@gmail.com>
2024-06-25 10:33:18 +0300

    Fix REQUIRED_ROLES

--- app.ts
+++ app.ts
@@ -8,7 +8,15 @@ import { cleanEnv, EnvError, makeValidator, str, url } from "envalid";
 
 const requiredRolesValidator = makeValidator((x) => {
   try {
-    return x.trim().split(",");
+    const trimmed = x.trim();
+
+    // Splitting empty string will result in [""], an array with one empty string
+    // So have to check for empty string here first
+    if (!trimmed) {
+      return [];
+    }
+
+    return trimmed.split(",");
   } catch (e) {
     throw new EnvError(`Invalid roles input: "${x}"`);
   }
@@ -25,7 +33,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 
 const isSessionAuthorized = async (
   sessionId: string,
-  requiredRoles: string[],
+  requiredRoles: string[]
 ) => {
   // If no roles are required, we can skip the query
   if (requiredRoles.length === 0) return true;
@@ -74,5 +82,5 @@ app.use(
         });
       },
     ],
-  }),
+  })
 );
